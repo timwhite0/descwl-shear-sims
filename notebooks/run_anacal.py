ae5c17396d41214f1bd0326bc4cce80ffe037345
Tim White <twhit@umich.edu>
2025-09-24 15:53:45 -0400

    Update AnaCal workflow

--- notebooks/run_anacal.py
+++ notebooks/run_anacal.py
@@ -10,7 +10,9 @@ from descwl_shear_sims.layout.layout import Layout
 
 import anacal
 
-os.environ['CATSIM_DIR'] = 'notebooks/catsim' 
+os.environ['CATSIM_DIR'] = 'catsim'
+
+
 
 def get_psf_param(psf_obj, pixel_scale, psf_size):
     center_pos = (psf_size / 2.0, psf_size / 2.0)
@@ -29,6 +31,8 @@ def get_psf_param(psf_obj, pixel_scale, psf_size):
         'psf_image': psf_array,
         'psf_object': psf_obj
     }
+
+
 
 def generate_one_image_and_catalog(
         rng, psf, layout, shifts, g1, g2,
@@ -96,6 +100,8 @@ def generate_one_image_and_catalog(
     
     return image_tensor, positions_tensor, M, magnitude
 
+
+
 def generate_many_images_and_catalogs(
         num_images, seed, layout_name, coadd_dim, buff, pixel_scale, density,
         select_observable, select_lower_limit, select_upper_limit,
@@ -159,34 +165,7 @@ def generate_many_images_and_catalogs(
     
     return images_tensor, catalogs, psf_params
 
-num_images = 2000
-seed = 1234
 
-
-psf_type = "moffat"          # "moffat" or "gauss"
-psf_size = 64
-psf_fwhm = 0.8              
-
-
-pixel_scale = 0.2            # 0.2 is default
-layout_name = 'random'       # 'random' or 'grid'
-coadd_dim = 2550             # without rotation, image dim is coadd_dim + 1
-buff = 0                     # 0 is default
-bands = ['i']                # ['i'] is default
-noise_factor = 1             # 0.58 is default
-dither = False               # False is default
-dither_size = 0.5            # 0.5 is default, but doesn't matter if dither = False
-rotate = False               # False is default
-cosmic_rays = False          # Set to True to add cosmic rays
-bad_columns = False          # Set to True to add bad columns
-
-density = 80                 # GALAXY density: per square arcmin, 80 is default  
-
-catalog_type = 'wldeblend'   # 'wldeblend' or 'fixed'   
-select_observable = 'i_ab'   # column name in the WLDEBlend catalog
-select_lower_limit = None
-select_upper_limit = 27
-save_dir = 'Anacal_results_density80'
 
 def convert_for_anacal(catalogs, psf_params):    
     g1_values = torch.tensor([cat['g1'] for cat in catalogs], dtype=torch.float32)
@@ -206,79 +185,81 @@ def convert_for_anacal(catalogs, psf_params):
         
     return anacal_catalog, anacal_psf
 
-def anacal_estimate(image, psf, catalog, npix):
+def anacal_single_image(image, psf, catalog, npix = 64, sigma_arcsec = 0.52, mag_zero = 30.0, pixel_scale = 0.2, noise_variance = 0.354, band = 0):
     catalog, psf = convert_for_anacal(catalog, psf)
-    true_shear1 = catalog['shear_1']
-    true_shear2 = catalog['shear_2']
 
     fpfs_config = anacal.fpfs.FpfsConfig(
         npix=npix,
-        sigma_arcsec=0.52,
+        sigma_arcsec=sigma_arcsec,
     )
 
-    mag_zero = 30.0
-    pixel_scale = 0.2
-    noise_variance = 0.354 # https://github.com/mr-superonion/xlens/blob/718794c0450565aec9c4f0e9d9082283e9fef5b6/xlens/simulator/multiband_defaults.py#L38
-    noise_array = torch.normal(mean = torch.zeros(2048, 2048), std = np.sqrt(noise_variance) * torch.ones(2048, 2048))
-    detection = None
+    gal_array = image[0,band]
+    psf_array = psf[0]['psf_image']
 
-    band = 0
+    out = anacal.fpfs.process_image(
+        fpfs_config=fpfs_config,
+        mag_zero=mag_zero,
+        gal_array=gal_array,
+        psf_array=psf_array,
+        pixel_scale=pixel_scale,
+        noise_variance=noise_variance,
+        noise_array=torch.normal(mean = torch.zeros(2048, 2048), std = np.sqrt(noise_variance) * torch.ones(2048, 2048)),
+        detection=None,
+    )
 
-    e1_sum = np.zeros(len(image))
-    e2_sum = np.zeros(len(image))
-    e1g1_sum = np.zeros(len(image))
-    e2g2_sum = np.zeros(len(image))
-    num_detections = np.zeros(len(image))
+    e1 = out["fpfs_w"] * out["fpfs_e1"]
+    e1g1 = out["fpfs_dw_dg1"] * out["fpfs_e1"] + out["fpfs_w"] * out["fpfs_de1_dg1"]
+    e1_sum = np.sum(e1)
+    e1g1_sum = np.sum(e1g1)
 
-    for i in range(len(image)):
-        gal_array = image[i,band]
-        psf_array = psf[i]['psf_image']
+    num_detections = len(e1)
 
-        out = anacal.fpfs.process_image(
-            fpfs_config=fpfs_config,
-            mag_zero=mag_zero,
-            gal_array=gal_array,
-            psf_array=psf_array,
-            pixel_scale=pixel_scale,
-            noise_variance=noise_variance,
-            noise_array=noise_array,
-            detection=detection,
-        )
+    e2 = out["fpfs_w"] * out["fpfs_e2"]
+    e2g2 = out["fpfs_dw_dg2"] * out["fpfs_e2"] + out["fpfs_w"] * out["fpfs_de2_dg2"]
+    e2_sum = np.sum(e2)
+    e2g2_sum = np.sum(e2g2)
 
-        e1 = out["fpfs_w"] * out["fpfs_e1"]
-        e1g1 = out["fpfs_dw_dg1"] * out["fpfs_e1"] + out["fpfs_w"] * out["fpfs_de1_dg1"]
-        e1_sum[i] = np.sum(e1)
-        e1g1_sum[i] = np.sum(e1g1)
+    return e1_sum, e1g1_sum, e2_sum, e2g2_sum, num_detections
 
-        num_detections[i] = len(e1)
 
-        e2 = out["fpfs_w"] * out["fpfs_e2"]
-        e2g2 = out["fpfs_dw_dg2"] * out["fpfs_e2"] + out["fpfs_w"] * out["fpfs_de2_dg2"]
-        e2_sum[i] = np.sum(e2)
-        e2g2_sum[i] = np.sum(e2g2)
 
-    e1_avg = e1_sum / num_detections
-    e2_avg = e2_sum / num_detections
-    R1 = np.sum(e1g1_sum) / np.sum(num_detections)
-    R2 = np.sum(e2g2_sum) / np.sum(num_detections)
+num_images = 2000
+seed = 42
+psf_type = "moffat"          # "moffat" or "gauss"
+psf_size = 64
+psf_fwhm = 0.8
+pixel_scale = 0.2            # 0.2 is default
+layout_name = 'random'       # 'random' or 'grid'
+coadd_dim = 2550             # without rotation, image dim is coadd_dim + 1
+buff = 0                     # 0 is default
+bands = ['i']                # ['i'] is default
+noise_factor = 1             # 0.58 is default
+dither = False               # False is default
+dither_size = 0.5            # 0.5 is default, but doesn't matter if dither = False
+rotate = False               # False is default
+cosmic_rays = False          # Set to True to add cosmic rays
+bad_columns = False          # Set to True to add bad columns
+density = 240                # GALAXY density: per square arcmin, 80 is default  
+catalog_type = 'wldeblend'   # 'wldeblend' or 'fixed'   
+select_observable = 'i_ab'   # column name in the WLDEBlend catalog
+select_lower_limit = None
+select_upper_limit = 27
 
-    est_shear1 = e1_avg / R1
-    est_shear2 = e2_avg / R2
 
-    return est_shear1, est_shear2, true_shear1, true_shear2
 
-est_shear1 = torch.zeros(num_images)
-est_shear2 = torch.zeros(num_images)
-true_shear1 = torch.zeros(num_images)
-true_shear2 = torch.zeros(num_images)
+save_dir = 'anacal_results_density240'
+
+
+
+e1_sum = torch.zeros(num_images)
+e1g1_sum = torch.zeros(num_images)
+e2_sum = torch.zeros(num_images)
+e2g2_sum = torch.zeros(num_images)
+num_detections = torch.zeros(num_images)
 
 images = []
 catalogs = []
 psf_params = []
-
-print(f"Starting generation of {num_images} images...")
-print(f"Bands: {bands}")
-print(f"Galaxy density: {density}")
 
 for j in tqdm(range(num_images), desc="Processing images"):
     im, cat, psfp = generate_many_images_and_catalogs(
@@ -295,18 +276,18 @@ for j in tqdm(range(num_images), desc="Processing images"):
     torch.save(catalogs, f'{save_dir}/catalogs.pt')
     torch.save(psf_params, f'{save_dir}/psf_params.pt') 
 
-    es1, es2, ts1, ts2 = anacal_estimate(im, psfp, cat, 64)
-    est_shear1[j] = es1
-    est_shear2[j] = es2
-    true_shear1[j] = ts1
-    true_shear2[j] = ts2
-
-    torch.save(est_shear1, f'{save_dir}/est_shear1.pt')
-    torch.save(est_shear2, f'{save_dir}/est_shear2.pt')
-    torch.save(true_shear1, f'{save_dir}/true_shear1.pt')
-    torch.save(true_shear2, f'{save_dir}/true_shear2.pt')
+    e1s, e1g1s, e2s, e2g2s, num_det = anacal_single_image(im, psfp, cat)
+    e1_sum[j] = e1s
+    e1g1_sum[j] = e1g1s
+    e2_sum[j] = e2s
+    e2g2_sum[j] = e2g2s
+    num_detections[j] = num_det
+    torch.save(e1_sum, f'{save_dir}/e1_sum.pt')
+    torch.save(e1g1_sum, f'{save_dir}/e1g1_sum.pt')
+    torch.save(e2_sum, f'{save_dir}/e2_sum.pt')
+    torch.save(e2g2_sum, f'{save_dir}/e2g2_sum.pt')
+    torch.save(num_detections, f'{save_dir}/num_detections.pt')
 
 print("All images processed successfully!")
-
 
 
